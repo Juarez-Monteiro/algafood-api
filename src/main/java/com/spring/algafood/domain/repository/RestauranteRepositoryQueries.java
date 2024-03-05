89f05f46ad5bce75b5b6fbddb050ac653787cd83
Juarez-Monteiro <juarezmonteiro_2@hotmail.com>
2024-03-04 22:07:07 -0300

    Estendendo o jpa repository para customizar o repositorio base

--- src/main/java/com/spring/algafood/domain/repository/RestauranteRepositoryQueries.java
+++ src/main/java/com/spring/algafood/domain/repository/RestauranteRepositoryQueries.java
@@ -8,4 +8,6 @@ import com.spring.algafood.domain.model.Restaurante;
 public interface RestauranteRepositoryQueries {
 	List<Restaurante> find(String nome, 
 			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+	
+	List<Restaurante> findComFreteGratis(String nome);
 }
